6923f912d5a2fefd110d69d253e484d3920a7eb9
tharunrede <therupallytharun@gmail.com>
2022-12-28 23:41:55 -0700

    Cht sheet

--- test.go
+++ test.go
@@ -12,9 +12,11 @@ func main() {
 	fmt.Println("Problem-5")
 
 	fmt.Println("Enter your String(Golang Training/ other): \n")
+
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	input_str := scanner.Text()
+
 	fmt.Println("Input value is: ", input_str)
 
 	fmt.Printf("type %T and value %v \n", input_str, input_str)
